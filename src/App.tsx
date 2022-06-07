77f5cb8bcacc443b531e063867eaeeb8ab309053
Shashank Baranwal <shashank.baranwal@shashankss418.successivetech.com>
2022-06-07 17:54:40 +0530

    Feature: Basic React Boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome 
+          {' '}
+          <code> To </code>
+          {' '}
+          Successive Technology.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
